c573d46ccdfef73f8e173c12b408a136fda842df
JCodeX-Dev <jcodex.dev@gmail.com>
2021-02-16 19:42:30 +0530

    Updated Entity Profile class

--- src/main/java/dev/jcodex/webapp/model/Profile.java
+++ src/main/java/dev/jcodex/webapp/model/Profile.java
@@ -1,17 +1,33 @@
 package dev.jcodex.webapp.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-@Entity(name = "profile")
+@Entity(name = "profiles")
 public class Profile {
 
     @Id
+    @Column(name = "profileID")
     private int id;
 
-    private String name;
+    private String first_name;
 
-    private String username;
+    private String last_name;
+
+    private String twitter_uname;
+
+    private String email;
+
+    private String facebook;
+
+    private String linkedin;
+
+    private String github;
+
+    private String quote;
+
+    private String about;
 
     public int getId() {
         return id;
@@ -21,28 +37,75 @@ public class Profile {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getFirst_name() {
+        return first_name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
     }
 
-    public String getUsername() {
-        return username;
+    public String getLast_name() {
+        return last_name;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
     }
 
-    @Override
-    public String toString() {
-        return "Profile{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", username='" + username + '\'' +
-                '}';
+    public String getTwitter_uname() {
+        return twitter_uname;
+    }
+
+    public void setTwitter_uname(String twitter_uname) {
+        this.twitter_uname = twitter_uname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFacebook() {
+        return facebook;
+    }
+
+    public void setFacebook(String facebook) {
+        this.facebook = facebook;
+    }
+
+    public String getLinkedin() {
+        return linkedin;
+    }
+
+    public void setLinkedin(String linkedin) {
+        this.linkedin = linkedin;
+    }
+
+    public String getGithub() {
+        return github;
+    }
+
+    public void setGithub(String github) {
+        this.github = github;
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public void setAbout(String about) {
+        this.about = about;
     }
 }
