a46aff6f298b1e535c06d7e7726afc9d53ce4ae5
JCodeX-Dev <jcodex.dev@gmail.com>
2021-06-13 19:26:02 +0530

    Added model class for Project section

---
+++ src/main/java/dev/jcodex/webapp/model/Project.java
@@ -0,0 +1,28 @@
+package dev.jcodex.webapp.model;
+
+public class Project {
+
+    private String title;
+
+    private String summary;
+
+    private String url;
+
+    private String image;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getImage() {
+        return image;
+    }
+}
