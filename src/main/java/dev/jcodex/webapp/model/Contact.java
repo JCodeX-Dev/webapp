f3b9f9d9de13629323bb345e57daac77c3f06873
JCodeX-Dev <jcodex.dev@gmail.com>
2021-06-13 19:24:53 +0530

    Added model class for Contact section

---
+++ src/main/java/dev/jcodex/webapp/model/Contact.java
@@ -0,0 +1,21 @@
+package dev.jcodex.webapp.model;
+
+public class Contact {
+    private String email;
+
+    private String phone;
+
+    private String location;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+}
