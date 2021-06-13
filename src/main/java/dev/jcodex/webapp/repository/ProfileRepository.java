2d8ae46b6e469108231b47c8acad8082eef2d8e8
JCodeX-Dev <jcodex.dev@gmail.com>
2021-06-13 19:29:56 +0530

    Updated repository class for Profile related queries

--- src/main/java/dev/jcodex/webapp/repository/ProfileRepository.java
+++ src/main/java/dev/jcodex/webapp/repository/ProfileRepository.java
@@ -1,9 +1,17 @@
 package dev.jcodex.webapp.repository;
 
+
+import dev.jcodex.webapp.model.Header;
 import dev.jcodex.webapp.model.Profile;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProfileRepository extends JpaRepository<Profile, Integer> {
+public interface ProfileRepository extends MongoRepository<Profile, ObjectId> {
+//    Profile findByUser(int user);
+
+    Profile findByProfileId(int user);
+
+//
 }
