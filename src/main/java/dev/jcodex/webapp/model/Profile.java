82577783cd18b21c7764ab5862908886d47df2bb
JCodeX-Dev <jcodex.dev@gmail.com>
2021-06-13 19:27:48 +0530

    Added model class for Profile

--- src/main/java/dev/jcodex/webapp/model/Profile.java
+++ src/main/java/dev/jcodex/webapp/model/Profile.java
@@ -1,114 +1,77 @@
 package dev.jcodex.webapp.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+//import javax.persistence.Column;
+//import javax.persistence.Entity;
+//import javax.persistence.Id;
 
-@Entity(name = "profiles")
-public class Profile {
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.util.List;
+
+//@Entity(name = "profiles")
+@Document(collection = "profiles")
+public final class Profile {
 
     @Id
-    @Column(name = "profileID")
-    private int id;
+    private String id;
 
-    @Column(name = "first_name")
-    private String firstName;
+    @Field("profile_id")
+    private int profileId;
 
-    @Column(name = "last_name")
-    private String lastName;
+    private Header header;
 
-    @Column(name = "twitter_uname")
-    private String twitterUname;
+    private Home home;
 
-    private String email;
+    private About about;
 
-    private String facebook;
+    private List<Skill> skills;
 
-    private String linkedin;
+//    private List<Qualification> qualifications;
 
-    private String github;
+    private List<Project> projects;
 
-    private String quote;
+    private List<Blog> blogs;
 
-    private String about;
+    private Contact contact;
 
-    public int getId() {
-        return id;
+    private Footer footer;
+
+    public Header getHeader() {
+        return header;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public Home getHome() {
+        return home;
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstNname(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getTwitterUname() {
-        return twitterUname;
-    }
-
-    public void setTwitterUname(String twitterUname) {
-        this.twitterUname = twitterUname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getFacebook() {
-        return facebook;
-    }
-
-    public void setFacebook(String facebook) {
-        this.facebook = facebook;
-    }
-
-    public String getLinkedin() {
-        return linkedin;
-    }
-
-    public void setLinkedin(String linkedin) {
-        this.linkedin = linkedin;
-    }
-
-    public String getGithub() {
-        return github;
-    }
-
-    public void setGithub(String github) {
-        this.github = github;
-    }
-
-    public String getQuote() {
-        return quote;
-    }
-
-    public void setQuote(String quote) {
-        this.quote = quote;
-    }
-
-    public String getAbout() {
+    public About getAbout() {
         return about;
     }
 
-    public void setAbout(String about) {
-        this.about = about;
+    public List<Skill> getSkills() {
+        return skills;
     }
+
+//    public List<Qualification> getQualification() {
+//        return qualifications;
+//    }
+
+    public List<Project> getProjects() {
+        return projects;
+    }
+
+    public List<Blog> getBlogs() {
+        return blogs;
+    }
+
+    public Contact getContact() {
+        return contact;
+    }
+
+    public Footer getFooter() {
+        return footer;
+    }
+
 }
