12ee0db6c57c31b31acc02003afa6035b6fb7b2a
JCodeX-Dev <jcodex.dev@gmail.com>
2021-06-25 21:18:45 +0530

    Added Mail Object Data Class

---
+++ src/main/java/dev/jcodex/webapp/model/Mail.java
@@ -0,0 +1,14 @@
+package dev.jcodex.webapp.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Mail {
+    private String name;
+    private String email;
+    private String subject;
+    private String message;
+
+}
